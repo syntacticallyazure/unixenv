f69f67e8fe3b69c813b3691cb701a12ac78ae2e5
syntacticallyazure <95119739+syntacticallyazure@users.noreply.github.com>
2026-08-09 13:30:51 +1000

    update to 26.05

--- src/shell.nix
+++ src/shell.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-26.05";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
@@ -21,5 +21,6 @@ pkgs.mkShellNoCC {
     delta
     ripgrep
     ripgrep-all
+    ssh-audit
   ];
 }
